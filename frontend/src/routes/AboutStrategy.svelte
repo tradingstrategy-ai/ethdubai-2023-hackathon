d6fd80cace967943ae1e5fb1175e40059a5bf671
Ken Kunz <kennethkunz@gmail.com>
2023-03-15 00:44:45 -0500

    fix typo

--- frontend/src/routes/AboutStrategy.svelte
+++ frontend/src/routes/AboutStrategy.svelte
@@ -8,10 +8,10 @@
 		>
 		is based on a
 		<a href="https://tradingstrategy.ai/glossary/portfolio-construction">portfolio construction</a> methodology.
-		It trade on Sushi DEX on Polygon blockchain. As a long-only strategy, it only trades in a bull market,
-		indicated by MATIC token 15 days simple moving average (SMA) price. The strategy identifies the top
-		three assets based on alpha signals and allocates them to a weighted portfolio using 1/nth method.
-		Stop loss is triggered at 3%, while profits are taken at 33%. The strategy maintain a 50% cash buffer,
-		and only invests in assets with a minimum available liquidity of 300k USD.
+		It trades using Sushi DEX on Polygon. As a long-only strategy, it only trades in a bull market, indicated
+		by MATIC token 15 days simple moving average (SMA) price. The strategy identifies the top three assets
+		based on alpha signals and allocates them to a weighted portfolio using 1/nth method. Stop loss is
+		triggered at 3%, while profits are taken at 33%. The strategy maintain a 50% cash buffer, and only
+		invests in assets with a minimum available liquidity of 300k USD.
 	</p>
 </section>
