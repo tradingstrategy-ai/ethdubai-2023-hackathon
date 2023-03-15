6dc3cfecb11cc9fc764a2b684b64202a94708f28
Ken Kunz <kennethkunz@gmail.com>
2023-03-15 00:32:06 -0500

    add backtesting summary metrics

--- frontend/src/routes/Backtest.svelte
+++ frontend/src/routes/Backtest.svelte
@@ -3,7 +3,32 @@
 </script>
 
 <section class="container backtest">
-	<h2>Backtest performance</h2>
+	<h2>Backtesting performance</h2>
+	<p>Backtesting was performed on historical Sushi DEX / Poloygon trading data.</p>
+
+	<h3>Backtesting key metrics</h3>
+
+	<table class="key-metrics">
+		<tr>
+			<th>Backtesting period</th>
+			<td>2022-01-01 - 2023-02-01 (396 days)</td>
+		</tr>
+		<tr>
+			<th>Annualised estimated return</th>
+			<td>64%</td>
+		</tr>
+		<tr>
+			<th>LP fees paid</th>
+			<td>~$1000 USD</td>
+		</tr>
+		<tr>
+			<th>Max pullback</th>
+			<td>-10%</td>
+		</tr>
+	</table>
+
+	<h3>Strategy benchmark</h3>
+
 	<p>Strategy performance compared to various buy-and-hold benchmarks:</p>
 	<ul>
 		<li><span style:color="#008800">▬</span> Strategy</li>
@@ -29,5 +54,21 @@
 	li span {
 		display: inline-block;
 		transform: scale(1.5, 1) translate(-2px, -1px);
+	}
+
+	table {
+		background: #eeeeee;
+		border-radius: 1rem;
+		border-collapse: separate;
+	}
+
+	th,
+	td {
+		padding: 0.5rem 1rem;
+	}
+
+	th {
+		text-align: left;
+		width: 30%;
 	}
 </style>
